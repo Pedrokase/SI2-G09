c6a264fd0e1467f416df0ee94a2d2478b99a6236
FredericaLeong <fredericaleong@hotmail.com>
2018-12-19 14:09:33 +0000

    ahhh

--- SI2-G09/SI2-G09/concrete/UtilizadorInstituicaoMapper.cs
+++ SI2-G09/SI2-G09/concrete/UtilizadorInstituicaoMapper.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using DAL;
+using DAL.mapper;
+using SI2_G09.mapper;
+using SI2_G09.model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SI2_G09.concrete
 {
-    class UtilizadorInstituicaoMapper
+    class UtilizadorInstituicaoMapper : AbstracMapper<UtilizadorInstituicao, int?, List<UtilizadorInstituicao>>, IUtilizadorInstituicaoMapper
     {
+        public UtilizadorInstituicaoMapper(IContext ctx) : base(ctx)
+        {
+        }
+
+        protected override string SelectAllCommandText => throw new NotImplementedException();
+
+        protected override string SelectCommandText => throw new NotImplementedException();
+
+        protected override string UpdateCommandText => throw new NotImplementedException();
+
+        protected override string DeleteCommandText => throw new NotImplementedException();
+
+        protected override string InsertCommandText => throw new NotImplementedException();
+
+        protected override void DeleteParameters(IDbCommand command, UtilizadorInstituicao e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InsertParameters(IDbCommand command, UtilizadorInstituicao e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override UtilizadorInstituicao Map(IDataRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SelectParameters(IDbCommand command, int? k)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override UtilizadorInstituicao UpdateEntityID(IDbCommand cmd, UtilizadorInstituicao e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateParameters(IDbCommand command, UtilizadorInstituicao e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- SI2-G09/SI2-G09/concrete/UtilizadorMapper.cs
+++ SI2-G09/SI2-G09/concrete/UtilizadorMapper.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿using DAL;
+using DAL.mapper;
+using SI2_G09.mapper;
+using SI2_G09.model;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SI2_G09.concrete
 {
-    class UtilizadorMapper
+    class UtilizadorMapper : AbstracMapper<Utilizador, int?, List<Utilizador>>, IUtilizadorMapper
     {
+        public UtilizadorMapper(IContext ctx) : base(ctx)
+        {
+        }
+
+        protected override string SelectAllCommandText => throw new NotImplementedException();
+
+        protected override string SelectCommandText => throw new NotImplementedException();
+
+        protected override string UpdateCommandText => throw new NotImplementedException();
+
+        protected override string DeleteCommandText => throw new NotImplementedException();
+
+        protected override string InsertCommandText => throw new NotImplementedException();
+
+        protected override void DeleteParameters(IDbCommand command, Utilizador e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void InsertParameters(IDbCommand command, Utilizador e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Utilizador Map(IDataRecord record)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void SelectParameters(IDbCommand command, int? k)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Utilizador UpdateEntityID(IDbCommand cmd, Utilizador e)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UpdateParameters(IDbCommand command, Utilizador e)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
